b71b72b594c82c9730cca29b0d7edc31e32b8717
Prelang Builder <builder@prelang.com>
2015-07-11 21:50:14 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Server' to allow for create, update

--- app/admin/server.rb
+++ app/admin/server.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Server do
+  permit_params :name, :address
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
