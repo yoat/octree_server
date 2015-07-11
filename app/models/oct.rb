d5b14047e88fd9fbce7e0439a0792e9959e968f9
yoat42 <yoat42@gmail.com>
2015-07-11 19:35:04 -0400

    Experimenting with associations

--- app/models/oct.rb
+++ app/models/oct.rb
@@ -1,5 +1,7 @@
 class Oct < ActiveRecord::Base
   belongs_to :player
-  belongs_to :oct
-  has_many :octs
+  belongs_to :parent, foreign_key: 'oct_id'
+  has_many :children, foreign_key: 'oct_id' #, -> { where root: false }
+  # http://www.postgresql.org/docs/8.3/static/datatype.html
+  # will either want fixed-length integers or bit[n] masks
 end
